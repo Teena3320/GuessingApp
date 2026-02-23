b4c14ed5a9dcef363e3f2dbc68831a3e408ecc0b
Teena3320 <teena.sebastian2022@vitstudent.ac.in>
2026-02-23 16:22:03 +0530

    Coded UC5-StorageService

--- GuessingApp.java
+++ GuessingApp.java
@@ -1,48 +1,56 @@
 /**
 * MAIN CLASS
 * 
-* Use case 4: Error Handling & Validation
+* Use case 5: Game Result Storage
 * 
-* This class coordinates the game execution while ensuring 
-* all user inputs are safely validated before processing.
+* This class coordinates the complete game flow
+* and persists the final result after completion.
 * 
 * Responsibilities:
 * - Initialize game configuration
-* - Accept user input
-* - Validate input using ValidationService
-* - Handle game flow without crashing on invalid input
+* - Accept and validate user guesses
+* - Generate hints when applicable
+* - Store game result at the end
 * 
 * @author Developer
-* @version 4.0
+* @version 5.0
 */
 
 import java.util.Scanner;
 
 public class GuessingApp {
 	public static void main(String[] args) throws InvalidInputException{
+		System.out.println("===========================");
 		System.out.println("Welcome to the Guessing App");
+		System.out.println("===========================\n");
+
+		System.out.print("Enter Player Name:");
+		Scanner scanner = new Scanner(System.in);
+		String player = scanner.nextLine();
+
 		GameConfig config = new GameConfig();
 		config.showRules();
 		
-		Scanner scanner = new Scanner(System.in);
+		boolean win = false;
+		
 		int attempts=0;
 		while(attempts < config.getMaxAttempts()){
 			System.out.print("Enter your guess: ");
 			int guess = ValidationService.validateInput(scanner.nextLine());
 			attempts++;
 			
-			
 			String result = GuessValidator.validateGuess(guess, config.getTargetNumber());
-			//String hint = ;
 			
-			if("!CORRECT".equals(result) && attempts < config.getMaxHints()){
+			if(!"CORRECT".equals(result) && attempts < config.getMaxHints()){
 				System.out.println(HintService.generateHint(config.getTargetNumber(), attempts));
 			}
 			System.out.println(result);
 			
 			if("CORRECT".equals(result)){
+				win = true;
 				break;
 			}
 		}
+		StorageService.saveResult(player,attempts,win);
 	}
 }
